011066d1027e4f57a9a77e4fedaf02aa17dfbc80
Pawan <paletipawankumar@gmail.com>
2023-12-14 21:55:29 +0530

    changed the icon and renamed the app

---
+++ android/app/src/main/kotlin/com/example/mymessages/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mymessages
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // functions for user login
+  void signIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
